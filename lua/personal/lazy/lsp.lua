60034a8c9305229ac3e68dacc6cb0889c6b647b4
Jeremiah Otoya <jeremiahmontoya@hey.com>
2026-01-06 10:41:51 -0800

    cleanup

--- lua/personal/lazy/lsp.lua
+++ lua/personal/lazy/lsp.lua
@@ -164,7 +164,7 @@ return {
         focusable = false,
         style = "minimal",
         border = "rounded",
-        source = "always",
+        source = true,
         header = "",
         prefix = "",
       },
