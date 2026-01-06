1a4578d0788c26f50cee7853fe91a9842bb91606
Jeremiah Otoya <jeremiahmontoya@hey.com>
2026-01-06 11:30:54 -0800

    improve trouble bg behavior

--- lua/personal/lazy/trouble.lua
+++ lua/personal/lazy/trouble.lua
@@ -3,11 +3,40 @@ return {
     "folke/trouble.nvim",
     config = function()
       require("trouble").setup({
-        icons = false,
+        icons = {
+          indent = {
+            middle = " ",
+            last = " ",
+            top = " ",
+            ws = "  ",
+          },
+          folder_closed = "",
+          folder_open = "",
+        },
+        use_diagnostic_signs = false,
+      })
+
+      -- Override Trouble's default highlight links to use Normal instead of NormalFloat
+      -- This fixes the black background issue with base16 themes
+      local function setup_trouble_highlights()
+        vim.api.nvim_set_hl(0, "TroubleNormal", { link = "Normal" })
+        vim.api.nvim_set_hl(0, "TroubleNormalNC", { link = "Normal" })
+      end
+
+      -- Apply immediately
+      setup_trouble_highlights()
+
+      -- Re-apply when colorscheme changes (since Trouble might reset them)
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = setup_trouble_highlights,
       })
 
       vim.keymap.set("n", "<leader>tt", function()
-        require("trouble").toggle()
+        require("trouble").toggle("diagnostics")
+      end)
+
+      vim.keymap.set("n", "<leader>td", function()
+        require("trouble").toggle("diagnostics", { filter = { buf = 0 } })
       end)
 
       vim.keymap.set("n", "[t", function()
